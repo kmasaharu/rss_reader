88199838de218b65bf1eeb5f00dc3b4242a1ef9a
unknown <r.1rou.1rou@gmail.com>
2014-03-22 09:11:36 +0900

    schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,6 +5,86 @@ every 1.day, :at => '4:30 am' do
   runner "Tasks::GetEntrys.execute"
 end
 
+every 1.day, :at => '1:00 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '1:30 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '2:00 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '2:30 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '3:00 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '3:30 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '4:00 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
 every 1.day, :at => '4:30 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '5:00 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '5:30 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '6:00 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '6:30 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '7:00 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '7:30 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '8:00 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '8:30 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '9:00 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '9:30 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '10:00 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '10:30 pm' do
+  runner "Tasks::GetEntrys.execute"
+end
+
+every 1.day, :at => '11:00 pm' do
   runner "Tasks::GetEntrys.execute"
 end
