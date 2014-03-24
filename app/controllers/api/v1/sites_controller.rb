3b8f2f7d09f6bad8d7568b92ac8cb57cda93de95
unknown <r.1rou.1rou@gmail.com>
2014-03-25 00:05:00 +0900

    sites jbuilder change

--- app/controllers/api/v1/sites_controller.rb
+++ app/controllers/api/v1/sites_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::SitesController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-      @sites = Site.find(:all)
+    url = request.url + params[:app_id].to_s;
+    @sites = Rails.cache.read(url)
+    if @sites.nil?
+      @sites = Site.find(:all, :conditions => {:app_id => params[:app_id]})
+      Rails.cache.write(url, @sites, expires_in: 60.minutes)  # 10分で消える。
+    end
   end
   
   def show
