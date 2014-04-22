5a342704cb9e1aa7bd79f1f304e746b9be331282
unknown <r.1rou.1rou@gmail.com>
2014-04-23 01:56:22 +0900

    schedule change

--- config/schedule.rb
+++ config/schedule.rb
@@ -5,15 +5,7 @@ every 1.day, :at => '4:30 am' do
   runner "Tasks::GetEntrys.execute"
 end
 
-every 1.day, :at => '1:00 pm' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '1:30 pm' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '2:00 pm' do
+every 1.day, :at => '11:00 am' do
   runner "Tasks::GetEntrys.execute"
 end
 
@@ -21,23 +13,7 @@ every 1.day, :at => '2:30 pm' do
   runner "Tasks::GetEntrys.execute"
 end
 
-every 1.day, :at => '3:00 pm' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '3:30 pm' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '4:00 pm' do
-  runner "Tasks::GetEntrys.execute"
-end
-
 every 1.day, :at => '4:30 pm' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '5:00 pm' do
   runner "Tasks::GetEntrys.execute"
 end
 
@@ -45,23 +21,7 @@ every 1.day, :at => '5:30 pm' do
   runner "Tasks::GetEntrys.execute"
 end
 
-every 1.day, :at => '6:00 pm' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '6:30 pm' do
-  runner "Tasks::GetEntrys.execute"
-end
-
 every 1.day, :at => '7:00 pm' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '7:30 pm' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '8:00 pm' do
   runner "Tasks::GetEntrys.execute"
 end
 
@@ -70,45 +30,5 @@ every 1.day, :at => '8:30 pm' do
 end
 
 every 1.day, :at => '9:00 pm' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '9:30 pm' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '10:00 pm' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '10:30 pm' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '11:00 pm' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '11:30 pm' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '00:00 am' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '00:30 am' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '01:00 am' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '01:30 am' do
-  runner "Tasks::GetEntrys.execute"
-end
-
-every 1.day, :at => '02:00 am' do
   runner "Tasks::GetEntrys.execute"
 end
