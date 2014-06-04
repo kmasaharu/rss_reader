6ac3387ee3e66e7f4b9c1cc02265007a6c7d1b67
kmasaharu <r.1rou.1rou@gmail.com>
2014-06-04 23:43:49 +0900

    cron error update

--- lib/tasks/get_soccer_entrys.rb
+++ lib/tasks/get_soccer_entrys.rb
@@ -1,24 +1,27 @@
 class Tasks::GetSoccerEntrys
   
   def self.execute
-    @sites   = Site.find(:all, :conditions => {:app_id => 5})
+    @sites   = Site.find(:all, :conditions => {:app_id => 4})
     
     @sites.each do |site|
+      puts 'rss url = ' + site.rss_url
       feeds = FeedNormalizer::FeedNormalizer.parse(open(site.rss_url), :force_parser => FeedNormalizer::SimpleRssParser)
       if !feeds.nil?
         # まずは古い　Entry　を消す（7日間保持する）
-        Entry.destroy_all("created_at <= '#{Time.mktime(Time.now.year, Time.now.month, Time.now.day - 5, 00, 00, 00)}'")
+        Entry.destroy_all("created_at <= '#{Time.mktime(Time.now.year, Time.now.month, Time.now.day - 3, 00, 00, 00)}'")
         feeds.entries.map do | feed |  
           if !feed.urls[0].nil?
+            puts '--feed url = ' + feed.urls[0]
             @entry = Entry.find(:all, :conditions => { :title => feed.title.force_encoding('utf-8') })
             if @entry.empty?
               @entry = Entry.new(:site_id => site.id, :title => feed.title.force_encoding('utf-8'), :url => feed.urls[0], :public_date => feed.date_published)
               @entry.save
+              puts '--OK'
             else
-              #puts 'Info exsit title'
+              puts '--Info exsit title'
             end
           else
-            #puts 'Error feed.urls nil'
+            puts '--Error feed.urls nil'
           end
         end
       end
